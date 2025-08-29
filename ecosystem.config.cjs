1a07eb81e980d7025f63c62d94718c99f6c5c7ce
JaredLiu <940101379@qq.com>
2025-08-29 16:56:50 +0800

    chore: update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'gpt-crawler-server',
-    script: 'dist/src/server.js',
+    script: './dist/src/server.js',
     instances: 1,
     autorestart: true,
     watch: false,
