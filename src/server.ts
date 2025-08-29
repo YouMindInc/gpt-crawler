119ab8d54ed136205c36d979b9153bfef136fac3
JaredLiu <940101379@qq.com>
2025-08-29 16:53:47 +0800

    chore: update

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send(`
     <!DOCTYPE html>
     <html>
